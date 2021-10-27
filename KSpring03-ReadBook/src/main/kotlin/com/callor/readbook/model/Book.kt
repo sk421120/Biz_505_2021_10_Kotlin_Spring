90e62630f727c4d85afbbf7cdeff413b3e291ada
sk421120 <sk421120@naver.com>
2021-10-27 17:07:30 +0900

    readbook

---
+++ KSpring03-ReadBook/src/main/kotlin/com/callor/readbook/model/Book.kt
@@ -0,0 +1,32 @@
+package com.callor.readbook.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_book", schema = "naraDB")
+data class Book(
+
+    @Id
+    @Column(
+        columnDefinition = "CHAR(13)",
+        nullable = false,
+        unique = true,
+        name = "isbn"
+    )
+    var isbn: String? = null,
+
+    @Column(nullable = false)
+    var title: String? = null,
+
+    @Column(nullable = false)
+    var comp: String? = null,
+
+    @Column(nullable = false)
+    var author: String? = null,
+
+    @Column(nullable = false)
+    var price: Int? = 0
+)
---
+++ KSpring03-ReadBook/src/main/kotlin/com/callor/readbook/model/ReadBook.kt
@@ -0,0 +1,35 @@
+package com.callor.readbook.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_readbook", schema = "naraDB")
+data class ReadBook(
+
+    @Id
+    @Column(
+        columnDefinition = "BIG INT",
+        nullable = false,
+        unique = true,
+        name = "seq"
+    )
+    var seq: Long? = null,
+
+    @Column(nullable = false)
+    var isbn: String? = null,
+
+    @Column(nullable = false)
+    var rdate: String? = null,
+
+    var stime: String? = null,
+
+    var etime: String? = null,
+
+    @Column(nullable = false)
+    var rating: Int? = 0,
+
+    var comment: String? = null
+)
